8653c4392eb5219153272fdb6c4f980a06d99f52
Sean Garnett <sean.garnett@protonmail.com>
2020-08-28 17:23:37 -0400

    Cleaned up some code, started making methods to rotate different faces of the cube

--- src/cubeStructure/Face.java
+++ src/cubeStructure/Face.java
@@ -3,22 +3,22 @@ package cubeStructure;
 // need to write a method to input all values of a face
 
 public class Face {
-	char[][] face = new char[3][3];
+	private char[][] face = new char[3][3];
 
 	public Face() {
-		// fills the array with ▆
-		for (int r = 0; r < 3; r++) {
-			for (int c = 0; c < 3; c++) {
-				this.face[r][c] = '\u2586';
+		// fills the array with ▆ '\u2586'
+		for (int r = 0; r < this.face.length; r++) {
+			for (int c = 0; c < this.face[0].length; c++) {
+				setIndex(r, c, '\u2586');
 			}
 		}
 	}
 
-	public Face(char var) {
-		// fills the array with given char
-		for (int r = 0; r < 3; r++) {
-			for (int c = 0; c < 3; c++) {
-				this.face[r][c] = var;
+	public Face(char tmp) {
+		// fills the array with given character
+		for (int r = 0; r < this.face.length; r++) {
+			for (int c = 0; c < this.face[0].length; c++) {
+				setIndex(r, c, tmp);
 			}
 		}
 
@@ -26,93 +26,117 @@ public class Face {
 
 	public void printSimple() {
 
-		for (int r = 0; r < 3; r++) {
-			for (int c = 0; c < 3; c++) {
+		for (int r = 0; r < this.face.length; r++) {
+			for (int c = 0; c < this.face[0].length; c++) {
 				System.out.print(this.face[r][c]);
-				if (c < 2) {
-					System.out.print(" ,");
-				} else if (c == 2) {
-					System.out.println();
-				}
+				System.out.print(", ");
 			}
+			System.out.println();
+
 		}
+
 		System.out.println();
 	}
 
 	public void printGrid() {
+
 		System.out.println("┌─┬─┬─┐");
 
-		for (int r = 0; r < 3; r++) {
-			System.out.print("│");
-			for (int c = 0; c < 3; c++) {
-				System.out.print(this.face[r][c]);
-				System.out.print("│");
-			}
+		for (int r = 0; r < this.face.length; r++) {
+			printRow(r);
+			System.out.println();
 
 			if (r < 2) {
-				System.out.println();
 				System.out.println("├─┼─┼─┤");
 			}
 		}
 
-		System.out.println();
 		System.out.print("└─┴─┴─┘");
 	}
 
 	public void printRow(int r) {
-		for (int c = 0; c < 3; c++) {
-			if (c == 0) {
-				System.out.print("│");
-			}
-			System.out.print(this.face[r][c]);
+		for (int c = 0; c < this.face[0].length; c++) {
 			System.out.print("│");
+			System.out.print(this.face[r][c]);
 		}
+		System.out.print("│");
 	}
 
-	public void set(int r, int c, char var) {
-		this.face[r][c] = var;
+	public void setIndex(int r, int c, char tmp) {
+		this.face[r][c] = tmp;
 	}
 
-	public void setRow() {
+	public void setRow(int r, char[] tmp) {
+		this.face[r] = tmp;
 
 	}
 
-	public void setColumn() {
+	public void setColumn(int c, char[] tmp) {
+		for (int r = 0; r < this.face.length; r++) {
+			this.face[r][c] = tmp[r];
+		}
 
+	}
+
+	public char getInxed(int r, int c) {
+		return this.face[r][c];
+	}
+
+	public char[] getRow(int r) {
+		return this.face[r];
+	}
+
+	public char[] getColumn(int c) {
+		char[] tmp = new char[3];
+
+		for (int r = 0; r < this.face.length; r++) {
+			tmp[r] = this.face[r][c];
+		}
+
+		return tmp;
+	}
+
+	public void invertRow(int r) {
+		char[] tmp = new char[3];
+
+		for (int c = 0; c < this.face[r].length; c++) {
+			tmp[c] = getRow(r)[2 - c];
+		}
+		setRow(r, tmp);
+	}
+
+	public void invertColumn(int c) {
+		char[] tmp = new char[3];
+		
+		for (int r = 0; r < this.face.length; r++) {
+			tmp[r]=getColumn(c)[2-r];
+		}
+		setColumn(c, tmp);
 	}
 
 	// 90 degree rotation clockwise
 	public void rotate() {
-		char[][] temp = new char[3][3];
+		char[][] tmp = new char[3][3];
 
-		for (int r = 0; r < 3; r++) {
-			for (int c = 0; c < 3; c++) {
-				temp[r][3 - 1 - c] = this.face[c][r];
+		for (int r = 0; r < this.face.length; r++) {
+			for (int c = 0; c < this.face[0].length; c++) {
+				tmp[r][this.face[0].length - 1 - c] = this.face[c][r];
 			}
 		}
 
-		for (int r = 0; r < 3; r++) {
-			for (int c = 0; c < 3; c++) {
-				this.face[r][c] = temp[r][c];
-			}
-		}
+		this.face = tmp;
 	}
 
 	// 90 degree rotation counterclockwise
 	public void counterRotate() {
-		char[][] temp = new char[3][3];
+		char[][] tmp = new char[3][3];
 
 		for (int r = 0; r < 3; r++) {
 			for (int c = 0; c < 3; c++) {
-				temp[3 - 1 - c][r] = this.face[r][c];
+				tmp[3 - 1 - c][r] = this.face[r][c];
 			}
 		}
-
-		for (int r = 0; r < 3; r++) {
-			for (int c = 0; c < 3; c++) {
-				this.face[r][c] = temp[r][c];
-			}
-		}
+		this.face = tmp;
 	}
 
 }
--- src/cubeStructure/RubiksCube.java
+++ src/cubeStructure/RubiksCube.java
@@ -7,17 +7,17 @@ public class RubiksCube {
 	static Face backF = new Face('b');
 	static Face leftF = new Face('l');
 	static Face topF = new Face('t');
-	static Face bottomF = new Face('b');
+	static Face bottomF = new Face('B');
+
+	static char[] tmpFront = new char[3];
+	static char[] tmpRight = new char[3];
+	static char[] tmpBack = new char[3];
+	static char[] tmpLeft = new char[3];
+	static char[] tmpTop = new char[3];
+	static char[] tmpBottom = new char[3];
 
 	public RubiksCube() {
 
-		/**
-		 * Face[] cube = new Face[6]; Instead of making the faces, I can make an array
-		 * of face objects. I can then map an array index to a face, IE Front Face will
-		 * be the first index. This could be potentially confusing and also requires the
-		 * printCube() method to be rewritten. However it allows a more automated
-		 * approach to manipulating the faces.
-		 */
 	}
 
 	public static void printCube() {
@@ -40,5 +40,55 @@ public class RubiksCube {
 
 		System.out.println("└─┴─┴─┘└─┴─┴─┘└─┴─┴─┘└─┴─┴─┘");
 		bottomF.printGrid();
+		System.out.println();
+	}
+
+	public static void setTMPRows(int face, int right, int back, int left, int top, int bottom) {
+		tmpFront = frontF.getRow(face);
+		tmpRight = rightF.getRow(right);
+		tmpBack = backF.getRow(back);
+		tmpLeft = leftF.getRow(left);
+		tmpTop = topF.getRow(top);
+		tmpBottom = bottomF.getRow(bottom);
+	}
+
+	public static void setTMPColumns(int face, int right, int back, int left, int top, int bottom) {
+		tmpFront = frontF.getColumn(face);
+		tmpRight = rightF.getColumn(right);
+		tmpBack = backF.getColumn(back);
+		tmpLeft = leftF.getColumn(left);
+		tmpTop = topF.getColumn(top);
+		tmpBottom = bottomF.getColumn(bottom);
+	}
+
+	public static void rotateFace() {
+		setTMPColumns(0, 0, 0, 2, 0, 0);
+	}
+
+	public static void rotateRight() {
+		setTMPColumns(2, 0, 0, 0, 2, 2);
+
+		topF.setColumn(2, tmpFront);
+		backF.setColumn(0, tmpTop);
+		backF.invertColumn(0);
+		bottomF.setColumn(2, tmpBack);
+		bottomF.invertColumn(2);
+		frontF.setColumn(2, tmpBottom);
+
+		rightF.rotate();
+	}
+
+	public static void iRotateRight() {
+		setTMPColumns(2, 0, 0, 0, 2, 2);
+
+		topF.setColumn(2, tmpBack);
+		topF.invertColumn(2);
+		backF.setColumn(0, tmpBottom);
+		backF.invertColumn(0);
+		bottomF.setColumn(2, tmpFront);
+		frontF.setColumn(2, tmpTop);
+
+		rightF.counterRotate();
+
 	}
 }
